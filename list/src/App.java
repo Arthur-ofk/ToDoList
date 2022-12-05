85d22169633caf2eb163f36f68043f47d0ef2e47
Arthur-ofk <arthauz19@gmail.com>
2022-12-05 16:40:59 +0200

    comit2

--- list/src/App.java
+++ list/src/App.java
@@ -1,78 +1,94 @@
-import assets.DeleteTaskController;
-import assets.AddTaskController;
-import assets.DataBaseHandler;
+import assets.TaskController;
+
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    DataBaseHandler dbh = new DataBaseHandler();
-    // java.util.Date utilDate = new java.util.Date();
-    System.out.println("select an option(or type help to view all commands)");
-    while (true) {
-      // System.out.println("select an option(or type help to view all commands)");
-      String option = sc.nextLine();
+    try (Scanner sc = new Scanner(System.in)) {
+      {
+        TaskController tc = new TaskController();
+        // java.util.Date utilDate = new java.util.Date();
+        System.out.println("select an option(or type help to view all commands)");
+        while (true) {
 
-      switch (option.toLowerCase()) {
-        case "":
-          System.out.println("select an option(or type help to view  all commands)");
-          break;
+          String option = sc.nextLine();
 
-        case "add task":
-          System.out.println("Enter Task Name(or short descr.)");
-          String TaskName = sc.nextLine();
-          System.out.println("Enter the deadline of Task(DD.MM.YYYY)");
+          switch (option.toLowerCase()) {
+            case "":
+              System.out.println("select an option(or type help to view all commands)");
+              option = null;
+              break;
 
-          String TaskDate = sc.nextLine();
+            case "add task":
+              System.out.println("Enter Task Name(or short descr.)");
+              String TaskName = sc.nextLine();
+              System.out.println("Enter the deadline of Task(YYYY-MM-DD)");
 
-          System.out.println("Enter the Task");
-          String TaskText = sc.next();
-          AddTaskController ATController = new AddTaskController(TaskName, TaskDate, TaskText);
-          ATController.signUp();
-          option = null;
-          break;
-        case "delete task":
-          System.out.println("Enter the Task Name You want to delete");
-          String delete = sc.next();
-          DeleteTaskController dtc = new DeleteTaskController(delete);
-          dtc.DeleteTask(delete);
-          break;
-        case "show all tasks":
+              String TaskDate = sc.nextLine();
+              System.out.println("Is this task finished?(true or false)");
+              Boolean IsFinished = sc.nextBoolean();
 
-          dbh.GetAllTasks();
-          break;
-        case "help":
-          System.out.println("List of all commands:");
-          System.out.println("Add Task - adds a new task to list");
-          System.out.println("Delete Task- remove a task from list");
-          System.out.println("Show All Tasks - prints all the tasks in list");
-          System.out.println("Close - exit the programm");
-          break;
-        case "sort":
-          System.out.println("Insert argument of sorting(possible input:)");
-          String argument = sc.next();
+              System.out.println("Enter the Task");
+              String TaskText = sc.next();
+              tc.AddTask(TaskName, TaskDate, TaskText, IsFinished);
 
-          dbh.SortTasks(argument);
-          break;
+              option = null;
+              break;
+            case "delete task":
+              System.out.println("Enter the Task Name You want to delete");
+              String delete = sc.next();
+              // DeleteTaskController dtc = new DeleteTaskController(delete);
+              tc.DeleteTask(delete);
+              break;
+            case "show all tasks":
 
-        case "search":
-          break;
+              tc.GetAllTasks();
+              break;
+            case "help":
+              System.out.println("List of all commands:");
+              System.out.println("Add Task - adds a new task to list");
+              System.out.println("Delete Task- remove a task from list");
+              System.out.println("Show All Tasks - prints all the tasks in list");
+              System.out.println("Close - exit the programm");
+              break;
+            case "sort":
+              System.out.println("Insert argument of sorting(possible input:name,date)");
+              String argument = sc.next();
 
-        case " update":
-          break;
+              tc.SortTasks(argument);
+              break;
 
-        case "finish":
-          break;
+            case "search":
+              System.out.println("search by:");
+              System.out.println("1)TaskName");
+              System.out.println("2)TaskDate");
+              String searchBy = sc.nextLine();
 
-        default:
+              System.out.println(" insert argument");
+              String sArg = sc.nextLine();
+              tc.SearchTask(searchBy, sArg);
 
-          System.out.println("Incorrect input");
-          break;
+              break;
 
+            case " update":
+              System.out.println("select task to update");
+              tc.GetAllTasks();
+
+              break;
+
+            case "finish":
+              break;
+
+            default:
+
+              System.out.println("Incorrect input");
+              break;
+
+          }
+
+          // option = null;
+        }
       }
-
-      // option = null;
     }
-
   }
 }
